3875b56c63c0513034b5ccb690b7ddb532568d6d
ampatspell <ampatspell@gmail.com>
2017-12-03 18:17:06 +0200

    cleanup

--- addon/-private/util/registry.js
+++ addon/-private/util/registry.js
@@ -2,24 +2,30 @@ import { A } from '@ember/array';
 import { copy } from '@ember/object/internals';
 
 export default class Registry {
+
   constructor() {
     this.keyed = Object.create(null);
     this.all = A();
   }
+
   get(key) {
     return this.keyed[key];
   }
+
   set(key, value) {
     this.keyed[key] = value;
     this.all.pushObject(value);
     return value;
   }
+
   remove(key) {
     let value = this.keyed[key];
     delete this.keyed[key];
     this.all.removeObject(value);
   }
+
   destroy() {
     copy(this.all).forEach(object => object.destroy());
   }
+
 }
