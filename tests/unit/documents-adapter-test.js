2d823a14d37ce9ce1af5007c8c4b3f5cbabff708
ampatspell <ampatspell@gmail.com>
2017-12-05 18:10:33 +0200

    test for documents stack

---
+++ tests/unit/documents-adapter-test.js
@@ -0,0 +1,25 @@
+import module from '../helpers/module-for-stores';
+import { test } from '../helpers/qunit';
+import Stores from 'documents/stores';
+
+const StoresImpl = Stores.extend({
+  storeOptionsForIdentifier(identifier) {
+    return {
+      adapter: 'couch',
+      url: 'http://127.0.0.1:5984'
+    };
+  }
+});
+
+module('documents-adapter');
+
+test('hello', async function(assert) {
+  this.register('documents:stores', StoresImpl);
+  let stores = this.lookup('documents:stores');
+  assert.ok(stores);
+  let store = stores.store('remote');
+  assert.ok(store);
+  let db = store.database('ember-cli-models');
+  let info = await db.get('documents').info();
+  assert.equal(info.db_name, 'ember-cli-models');
+});
