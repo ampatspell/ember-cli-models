44fba8ad850ce63f78e9f9b40b9b86b77282da1b
Arnis Vuskans <ampatspell@gmail.com>
2018-01-05 16:22:34 +0200

    dummy

--- tests/dummy/app/components/ui-route/index/component.js
+++ tests/dummy/app/components/ui-route/index/component.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import layout from './template';
-import { stores, store, database, loader } from 'ember-cli-models/computed';
+import { stores, store, database } from 'ember-cli-models/computed';
 import { state } from 'dummy/models/state';
 
 export default Component.extend({
@@ -12,36 +12,12 @@ export default Component.extend({
   database: database('remote', 'main'),
   state: state(),
 
-  ddoc: null,
-  view: null,
-  autoload: false,
-
-  loader: loader(function(owner, stores) {
-    return {
-      recurrent: false,
-      owner: [ 'ddoc', 'view' ],
-      perform() {
-        return stores.database('remote', 'main').find({ all: true }).then(models => {
-          return {
-            isMore: false,
-            state: {
-              loaded: models.get('length')
-            }
-          };
-        });
-      }
-    };
-  }),
-
   init() {
     this._super(...arguments);
     window.component = this;
   },
 
   actions: {
-    enableAutoload() {
-      this.set('autoload', true);
-    },
     async setup() {
       let setup = await this.get('state.design').recreate();
       this.set('setup', setup);
