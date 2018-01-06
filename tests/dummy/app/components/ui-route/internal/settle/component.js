ed9187790b837ac9da69a1116e205fb3a78184f1
Arnis Vuskans <ampatspell@gmail.com>
2018-01-06 11:26:42 +0200

    dummy

--- tests/dummy/app/components/ui-route/internal/settle/component.js
+++ tests/dummy/app/components/ui-route/internal/settle/component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 import layout from './template';
 import { view, filterByType } from 'dummy/models/-computed-remote';
+import { getStores } from 'ember-cli-models/util';
+import { info } from 'ember-cli-models/-private/util/logger';
 
 export default Component.extend({
   classNameBindings: [ ':ui-route-internal-settle' ],
@@ -11,5 +13,14 @@ export default Component.extend({
 
   blogsLoader: view({ ddoc: 'main', view: 'by-type', key: 'blog' }),
   blogs: filterByType({ type: 'blog', new: false }),
+
+  init() {
+    this._super(...arguments);
+    let stores = getStores(this);
+    stores.settle().then(() => {
+      info('authors', this.$('[data-test-authors]').data('test-authors'));
+      info('blogs', this.$('[data-test-blogs]').data('test-blogs'));
+    });
+  }
 
 });
