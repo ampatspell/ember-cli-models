442d9047d1e187618a69d2b8afeb1bf52b4b0eab
Arnis Vuskans <ampatspell@gmail.com>
2018-01-06 14:54:09 +0200

    operation cancel base impl

--- addon/-private/util/operation/operation.js
+++ addon/-private/util/operation/operation.js
@@ -3,6 +3,10 @@ import { next } from '../promise';
 
 export default class Operation {
 
+  constructor() {
+    this.cancelled = false;
+  }
+
   // perform() {
   // }
 
@@ -11,6 +15,10 @@ export default class Operation {
       .then(() => this.perform())
       .then(arg => this._deferred.resolve(arg), err => this._deferred.reject(err));
     return this.promise;
+  }
+
+  cancel() {
+    this.cancelled = true;
   }
 
   get _deferred() {
