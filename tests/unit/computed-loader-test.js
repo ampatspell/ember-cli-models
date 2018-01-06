0aab14ca86ad2f2ef4437057274ff224f90a1118
Arnis Vuskans <ampatspell@gmail.com>
2018-01-06 14:13:28 +0200

    few autoload tests

--- tests/unit/computed-loader-test.js
+++ tests/unit/computed-loader-test.js
@@ -4,6 +4,7 @@ import EmberObject from '@ember/object';
 import { loader } from 'ember-cli-models/computed';
 import { resolve } from 'rsvp';
 import { next } from 'ember-cli-models/-private/util/promise';
+import ModelsError from 'ember-cli-models/-private/util/error';
 
 module('computed-loader', {
   beforeEach() {
@@ -12,17 +13,23 @@ module('computed-loader', {
     this.subject = () => {
       this.register('app:subject', EmberObject.extend({
         id: 'duck',
+        error: null,
         loader: loader(function() {
           return {
             recurrent: false,
-            owner: [ 'id' ],
-            perform() {
-              let id = this.get('id');
-              log.push(`load ${id}`);
-              return resolve()
-                .then(() => next())
-                .then(() => next())
-                .then(() => {});
+            owner: [ 'id', 'error' ],
+            async perform() {
+              await next();
+              await next();
+
+              let { id, error } = this.getProperties('id', 'error');
+              if(error) {
+                log.push(`error ${id} ${error}`);
+                throw new ModelsError({ error: 'perform', reason: error });
+              } else {
+                log.push(`load ${id}`);
+                return { id };
+              }
             }
           };
         })
@@ -155,4 +162,32 @@ test('autoload and reload', async function(assert) {
 
   await one;
   await two;
+});
+
+test('loader.autoload', async function(assert) {
+  let subject = this.subject();
+  let loader = subject.get('loader');
+
+  assert.equal(loader.get('autoload.isLoading'), true);
+
+  await this.stores.settle();
+
+  assert.equal(loader.get('autoload.isLoading'), false);
+});
+
+test('loader.autoload does not load after error', async function(assert) {
+  let subject = this.subject();
+  subject.set('error', 'not found');
+  let loader = subject.get('loader');
+
+  assert.equal(loader.get('autoload.isLoading'), true);
+
+  await this.stores.settle();
+
+  assert.deepEqual(loader.get('error').toJSON(), {
+    "error": "perform",
+    "reason": "not found"
+  });
+
+  assert.equal(loader.get('autoload.isLoading'), false);
 });
